9020b1bdba49f7df391698549f1e0c684850fc95
fvoronov <f.voronov@kse.org.ua>
2026-07-06 13:15:40 +0300

    created save and load functions in storage.py

---
+++ storage.py
@@ -0,0 +1,19 @@
+import csv
+
+from models import Expense
+
+
+def save(expenses, filename="expenses.csv"):
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["id", "date", "category", "amount", "description"])
+        for expense in expenses:
+            writer.writerow([expense.id, expense.date, expense.category, expense.amount, expense.description])
+
+def load(filename="expenses.csv"):
+    expenses = []
+    with open(filename, "r", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            expenses.append(Expense(int(row["id"]), row["date"], row["category"], float(row["amount"]), row["description"]))
+        return expenses
